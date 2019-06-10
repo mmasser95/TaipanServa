91592c2baa0e28db1aa0233160acafac019d6628
mmasser95 <mmasser95@gmail.com>
2019-06-11 00:00:54 +0200

    xDDDD

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    DB: process.env.DB,
+    SECRET: process.env.SECRET,
+    PORT: process.env.PORT || 3000
+  };
+  
+
+  module.exports = {
+    DB: "mongodb://chattabutt:chattabutt1@ds135107.mlab.com:35107/chattabutt",
+    SECRET: "74159634185637418596378529852",
+    PORT: process.env.PORT || 3000
+  };
